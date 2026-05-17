0aee1093e3524077c5e27be1fca5369ee8532279
Paul Niziolek <niziolek.paul@gmail.com>
2026-05-17 17:44:15 -0400

    hittable: new ray colliding with hittable things abstraction

---
+++ hittable.h
@@ -0,0 +1,20 @@
+#ifndef SIMPLERAYTRACING_HITTABLE_H
+#define SIMPLERAYTRACING_HITTABLE_H
+
+#include "ray.h"
+
+class hit_record {
+public:
+    point3 p;
+    vec3 normal;
+    double t;
+};
+
+class hittable {
+public:
+    virtual ~hittable() = default;
+
+    virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+};
+
+#endif //SIMPLERAYTRACING_HITTABLE_H
