f48ee3d6ed2ff788072d5373a1d869eb0bbba568
Paul Niziolek <niziolek.paul@gmail.com>
2026-05-17 21:04:02 -0400

    camera: fix small floating point imprecision bugs causing shadow acne

--- camera.h
+++ camera.h
@@ -96,7 +96,8 @@ private:
 
         hit_record rec;
 
-        if (world.hit(r, interval(0, infinity), rec)) {
+        // Hack: fixing float imprecision causing shadow acne with arbitrarily small t_min.
+        if (world.hit(r, interval(0.001, infinity), rec)) {
             vec3 direction = random_on_hemisphere(rec.normal);
             return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
         }
