78c820d544a07c4cf6c04e4fdda1c839bb84b771
Paul Niziolek <niziolek.paul@gmail.com>
2026-05-17 20:47:41 -0400

    vec: add ray bouncing

--- camera.h
+++ camera.h
@@ -2,6 +2,7 @@
 #define SIMPLERAYTRACING_CAMERA_H
 
 #include "hittable.h"
+#include "vec3.h"
 
 class camera {
 public:
@@ -91,7 +92,8 @@ private:
     color ray_color(const ray& r, const hittable& world) const {
         hit_record rec;
         if (world.hit(r, interval(0, infinity), rec)) {
-            return 0.5 * (rec.normal + color(1, 1, 1));
+            vec3 direction = random_on_hemisphere(rec.normal);
+            return 0.5 * ray_color(ray(rec.p, direction), world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
