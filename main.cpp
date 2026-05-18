ec649aa8d2dce97bfc080faa0ecbdd07670c322b
Paul Niziolek <niziolek.paul@gmail.com>
2026-05-18 00:22:37 -0400

    main: zoom in to scene

--- main.cpp
+++ main.cpp
@@ -9,8 +9,6 @@
 int main() {
     hittable_list world;
 
-    auto R = std::cos(pi/4);
-
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left   = make_shared<dielectric>(1.50);
@@ -29,7 +27,7 @@ int main() {
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
 
-    cam.vfov = 90;
+    cam.vfov = 20;
     cam.lookfrom = point3(-2, 2, 1);
     cam.lookat = point3(0, 0, -1);
     cam.vup = vec3(0, 1, 0);
