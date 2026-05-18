ae22fe292d3fe31c880d2d8384cc20541f5fa72a
Paul Niziolek <niziolek.paul@gmail.com>
2026-05-17 23:57:11 -0400

    material add glass-like reflectance to dielectrics

--- material.h
+++ material.h
@@ -64,7 +64,7 @@ public:
 
         bool cannot_refract = ri * sin_theta > 1.0;
         vec3 direction;
-        if (cannot_refract)
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double())
             direction = reflect(unit_direction, rec.normal);
         else
             direction = refract(unit_direction, rec.normal, ri);
@@ -75,6 +75,13 @@ public:
 
 private:
     double refraction_index;
+
+    static double reflectance(double cosine, double refraction_index) {
+        // Reflectance is calculated by Shlick's approximation.
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+    }
 };
 
 #endif //SIMPLERAYTRACING_MATERIAL_H
